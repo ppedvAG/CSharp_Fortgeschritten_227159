429c5493740144f99f5471f85b4b79060a28e398
ppedvLukasK <lukask@ppedv.de>
2023-05-15 16:08:52 +0200

    M004

---
+++ Generics/Constraints.cs
@@ -0,0 +1,40 @@
+﻿namespace Generics;
+
+public class Constraints
+{
+	static void Main(string[] args)
+	{
+		DataStore5<DayOfWeek> dw;
+		//DataStore5<DayOfWeek.Sunday> dw1;
+	}
+
+	public void Test<MyType>() where MyType : Constraints //Constraints bei Methode hinzufügen
+	{
+
+	}
+}
+
+
+public class DataStore1<T> where T : class { } //T muss ein Referenztyp sein (string, object, ...)
+
+public class DataStore2<T> where T : struct { } //T muss ein Wertetyp sein (int, bool, ...)
+
+public class DataStore3<T> where T : Constraints { } //T muss die gegebene Klasse selber oder eine Unterklasse davon sein
+
+public class DataStore4<T> where T : new() { } //T muss einen Standardkonstruktor haben
+
+public class DataStore5<T> where T : Enum { } //T muss ein Enum sein (kein Enumwert)
+
+public class DataStore6<T> where T : Delegate { } //T muss ein Delegate sein
+
+public class DataStore7<T> where T : unmanaged { }
+//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/unmanaged-types
+
+public class DataStore8<T> where T : class, Enum, new() { } //Mehrere Constraints auf ein Generic
+
+public class DataStore9<T1, T2> //Mehrere Constraints auf mehrere Generics
+	where T1 : class
+	where T2 : struct
+{
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Generics;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		List<int> list = new List<int>(); //Generic: T wird nach unten übernommen (hier T = int)
+		list.Add(1); //T wird durch int ersetzt
+
+		List<string> listStr = new();
+		listStr.Add("123");
+
+		Dictionary<string, int> dict = new(); //Klasse mit 2 Generics: TKey -> string, TValue -> int
+		dict.Add("123", 123); //Add(TKey, TValue) -> Add(string, int)
+	}
+}
+
+public class DataStore<T> :
+	IProgress<T>, //T bei Vererbung weitergeben
+	IEquatable<int> //Fixen Typ bei Vererbung übergeben
+{
+	private T[] data { get; set; } //T bei einem Feld/Property
+
+	public List<T> Data => data.ToList(); //Generic wird nach unten gegeben
+
+	public void Add(int index, T item) //T bei Parameter
+	{
+		data[index] = item;
+	}
+
+	public T Get(int index)
+	{
+		if (index < 0 || index > data.Length)
+			return default(T); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		return data[index];
+	}
+
+	public void Report(T value) //T kommt von Interface
+	{
+
+	}
+
+	public bool Equals(int other) //Fixer Typ kommt von oben
+	{
+		return true;
+	}
+
+	public void PrintType<MyType>()
+	{
+        Console.WriteLine(default(MyType)); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+        Console.WriteLine(typeof(MyType)); //Typ aus Generic holen
+        Console.WriteLine(nameof(MyType)); //Typ als string (z.B. "int", "string", "Func", ...)
+
+        //if (MyType is int) { }
+        if (typeof(MyType) == typeof(int)) { } //Typvergleich mit Generic
+	}
+}
+
+public class DataStoreVererbung<T> : DataStore<T> { }
